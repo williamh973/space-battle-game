18b2deb61897d2c2fa20fe264f709f70cac380e0
williamh973 <126678879+williamh973@users.noreply.github.com>
2024-03-10 12:00:23 +0100

    add  files animate.js, auto-shoot.js, canvas-parameters.js et game-variables.js after renamed

---
+++ auto-shoot.js
@@ -0,0 +1,29 @@
+import { gameVariables } from "./game-variables.js";
+import { increaseHudMissileShooted } from "./interface.js";
+
+
+
+export const startAutoShootIfModActived = () => {
+    if (gameVariables.isAutomaticalShootMod) {
+        autoShoot();
+    }
+};
+
+export const manualShoot = () => {
+    if (!gameVariables.isAutomaticalShootMod) {
+        gameVariables.player.shoot();
+        increaseHudMissileShooted();
+    } else {
+        return;
+    }
+ };
+
+const autoShoot = () => {
+   setInterval(() => {
+       gameVariables.player.shoot();
+       increaseHudMissileShooted();
+   }, 500);
+
+};
+
+ 
